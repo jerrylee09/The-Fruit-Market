e9c0bb8b492bee82e57478dc41c069aac5c63b1f
Jerry Lee <jerry.lee09@yahoo.com>
2016-08-04 11:11:02 -0500

    edit js

--- script.js
+++ script.js
@@ -4,61 +4,118 @@ $(document).ready(function() {
 		oranges: 0,
 		bananas: 0,
 		grapes: 0,
-		totalCash: 100
+		totalCash: 10
 	};
-	$(".price-button").on("click","button",targetBuy);
-	// $(".price-button").on("click","button",targetSell);
+	$(".price-button").on("click","button.buy",targetBuy);
+	$(".price-button").on("click","button.sell",targetSell);
 
 var fruitPrices = [1,1,1,1];
 var fruitTimes = [0,0,0,0];
 
-var apple =	$('#apple-price').text(fruitPrices[0]);
-var banana = $('#banana-price').text(fruitPrices[1]);
-var orange = $('#orange-price').text(fruitPrices[2]);
-var grape =	$('#grape-price').text(fruitPrices[3]);
+var apple = fruitPrices[0];
+var banana = fruitPrices[1];
+var orange = fruitPrices[2];
+var grape = fruitPrices[3];
+
+
+var applePrice = $('#apple-price').text(apple);
+var bananaPrice = $('#banana-price').text(banana);
+var orangePrice = $('#orange-price').text(orange);
+var grapePrice = $('#grape-price').text(grape);
 
 $(".total-cash").text(customer.totalCash);
 
-
 function targetBuy(){
 	 var myClass = this.className;
+	 console.log(customer.totalCash);
+	 $(".total-cash").text(customer.totalCash);
 
-	 if (myClass ==="apple-buy"){
+	 if(customer.totalCash <= 0 || customer.totalCash <= fruitPrices[0] || customer.totalCash <= fruitPrices[1] || customer.totalCash <= fruitPrices[2]|| customer.totalCash <= fruitPrices[3]){
+	 		console.log(customer.totalCash);
+			customer.totalCash = 0;
+			console.log("you hit me)");
+		}
+		else if (myClass === "btn btn-default btn-lg  apple-buy buy"){
 	 	customer.apples = customer.apples+1 ;
-		customer.totalCash -= fruitPrices[0]; 	 
-	 }
-	 else if(myClass === "banana-buy"){
+		customer.totalCash -= fruitPrices[0]; 
+		$('#apple-total').text(customer.apples);
+		$(".total-cash").text(customer.totalCash);
+		
+	 	}
+	 	else if(myClass === "btn btn-default btn-lg banana-buy buy"){
 	 	customer.bananas += 1;
-	 	console.log(customer.bananas);
-	 	customer.totalCash -= fruitPrices[1]; 
-	 }
-	 else if(myClass === "orange-buy"){
+	 	customer.totalCash -= fruitPrices[1];  
+	 	$('#banana-total').text(customer.bananas);
+	 	$(".total-cash").text(customer.totalCash);
+	 	
+	 	}
+	 	else if(myClass === "btn btn-default btn-lg orange-buy buy"){
 	 	customer.oranges += 1;
-	 	console.log(customer.oranges);
 	 	customer.totalCash -= fruitPrices[2]; 
-	 }
-	 else if(myClass === "grape-buy"){
+	 	$('#orange-total').text(customer.oranges);
+	 	$(".total-cash").text(customer.totalCash);
+	 		 
+	 	}
+	 	else if(myClass === "btn btn-default btn-lg grape-buy buy"){
 	 	customer.grapes += 1;
-	 	console.log(customer.grapes);
-	 	customer.totalCash -= fruitPrices[3]; 
+	 	customer.totalCash -= fruitPrices[3];
+	 	$('#grape-total').text(customer.grapes);
+	 	$(".total-cash").text(customer.totalCash);
+	 	
 	 }
-
 };
 
 $
-// function targetSell(){
+function targetSell(){
+	console.log("You clicked");
 
-
+	var myClass = this.className;
+	if(customer.apples === 0 || customer.bananas === 0 || customer.oranges === 0|| customer.grapes === 0){
+	 		console.log(customer.totalCash);
+			console.log("you hit me)");
+		}
+		else if (myClass === "btn btn-default btn-lg  apple-buy buy"){
+	 	customer.apples = customer.apples-1 ;
+		customer.totalCash += fruitPrices[0]; 
+		$('#apple-total').text(customer.apples);
+		$(".total-cash").text(customer.totalCash);
+		console.log("meow");
+		
+	 	}
+	 	else if(myClass === "btn btn-default btn-lg banana-buy buy"){
+	 	customer.bananas -= 1;
+	 	customer.totalCash += fruitPrices[1];  
+	 	$('#banana-total').text(customer.bananas);
+	 	$(".total-cash").text(customer.totalCash);
+	 	
+	 	}
+	 	else if(myClass === "btn btn-default btn-lg orange-buy buy"){
+	 	customer.oranges -= 1;
+	 	customer.totalCash += fruitPrices[2]; 
+	 	$('#orange-total').text(customer.oranges);
+	 	$(".total-cash").text(customer.totalCash);
+	 		 
+	 	}
+	 	else if(myClass === "btn btn-default btn-lg grape-buy buy"){
+	 	customer.grapes -= 1;
+	 	customer.totalCash += fruitPrices[3];
+	 	$('#grape-total').text(customer.grapes);
+	 	$(".total-cash").text(customer.totalCash);
+	 	
+	 }
+};
 
 function priceChange(){
 	for (var i = 0; i < fruitTimes.length;i++){
 	fruitTimes[i] = (randomNumber(-50,50)/100);
 	// (fruitTimes[i].toFixed(2))/1;
 	// console.log(fruitTimes);
+
 	}
+
 };
 
-
+	
 function priceSet(){
 	for(var i = 0; i < fruitPrices.length; i++){
 		fruitPrices[i] += fruitTimes[i];
@@ -66,21 +123,50 @@ function priceSet(){
 	console.log(fruitPrices);
 };
 
-
-
-
-
-
+function timerSet() {
 setInterval(function(){ 
 	priceChange();
 	priceSet();
 
- 	apple.text(fruitPrices[0].toFixed(2));
- 	banana.text(fruitPrices[1].toFixed(2));
-	orange.text(fruitPrices[2].toFixed(2));
-	grape.text(fruitPrices[3].toFixed(2));
+if(fruitPrices[0] < 0.50) {
+	fruitPrices[0] = 0.50;
+	} else if(fruitPrices[0] > 9.99) {
+	fruitPrices[0] = 9.99;
+	}
 
- }, 3000);
+	if(fruitPrices[1] < 0.50) {
+	fruitPrices[1] = 0.50;
+	} else if(fruitPrices[1] > 9.99) {
+	fruitPrices[1] = 9.99;
+	}
+
+	if(fruitPrices[2] < 0.50) {
+	fruitPrices[2] = 0.50;
+	} else if(fruitPrices[2] > 9.99) {
+	fruitPrices[2] = 9.99;
+	}
+
+	if(fruitPrices[3] < 0.50) {
+	fruitPrices[3] = 0.50;
+	} else if(fruitPrices[3] > 9.99) {
+	fruitPrices[3] = 9.99;
+	}
+
+ 	applePrice.text(fruitPrices[0].toFixed(2));
+ 	bananaPrice.text(fruitPrices[1].toFixed(2));
+	orangePrice.text(fruitPrices[2].toFixed(2));
+	grapePrice.text(fruitPrices[3].toFixed(2));
+
+ }, 100);
+}
+
+//timerSet();
+
+
+// setInterval(function(){ 
+
+//  }, 300000);
+
 
 function randomNumber(min, max) {
 	return Math.floor(Math.random() * (1 + max - min) + min);
